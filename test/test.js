6cd231514b6f9c3b28011e2e6eb219a323196f36
Denys Korolkov <denys@korolkov.pro>
2018-08-31 15:07:32 -0400

    ADD 2 tests

--- test/test.js
+++ test/test.js
@@ -5,6 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const assert_1 = __importDefault(require("assert"));
 const parser_1 = __importDefault(require("../src/parser"));
+//   console.log(postFixExec([]));
+//   console.log(postFixExec([3]));
+//   console.log(postFixExec([2, 3]));
+//   console.log(postFixExec(['+']));
+//   console.log(postFixExec(['+', 3]));
+//   console.log(postFixExec([3, 4, '+']));
+//   console.log(postFixExec([5, '+', 2, '+', 4, '*', '+', 3, '-']));
+//   console.log(postFixExec([5, 1, 2, '+', 4, '*', '+', 3, '-']));
+//   console.log(postFixExec([5, 1, 2, '+', 4, '*', '+', 3]));
 describe('CSV parser with postfix', function () {
     describe('postfix', function () {
         it('1+2', function () {
@@ -36,12 +45,26 @@ describe('CSV parser with postfix', function () {
         });
     });
     describe('parser', function () {
-        it('linked', function () {
+        it('linked forward', function () {
             assert_1.default.deepEqual(parser_1.default(`b2,,1
 1,c3,1
 1,1,5`), `5,0,1
 1,5,1
 1,1,5`);
+        });
+        it('linked back', function () {
+            assert_1.default.deepEqual(parser_1.default(`5,,1
+1,a1,1
+1,1,b2`), `5,0,1
+1,5,1
+1,1,5`);
+        });
+        it('sample', function () {
+            assert_1.default.deepEqual(parser_1.default(`b1 b2 +,2 b2 3 * -,3 ,+
+a1,5 ,,7 2/
+c2 3*,1 2 , ,5 1 2+4*+3-`), `-8,-13,3,#ERR
+-8,5,0,5
+0,#ERR,0,14`);
         });
     });
 });
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,16 @@
 import assert from 'assert';
 import parser from '../src/parser';
 
+//   console.log(postFixExec([]));
+//   console.log(postFixExec([3]));
+//   console.log(postFixExec([2, 3]));
+//   console.log(postFixExec(['+']));
+//   console.log(postFixExec(['+', 3]));
+//   console.log(postFixExec([3, 4, '+']));
+//   console.log(postFixExec([5, '+', 2, '+', 4, '*', '+', 3, '-']));
+//   console.log(postFixExec([5, 1, 2, '+', 4, '*', '+', 3, '-']));
+//   console.log(postFixExec([5, 1, 2, '+', 4, '*', '+', 3]));
+
 describe('CSV parser with postfix', function() {
   describe('postfix', function() {
     it('1+2', function() {
@@ -40,7 +50,7 @@ describe('CSV parser with postfix', function() {
     });
   });
   describe('parser', function() {
-    it('linked', function() {
+    it('linked forward', function() {
       assert.deepEqual(
         parser(
           `b2,,1
@@ -50,6 +60,30 @@ describe('CSV parser with postfix', function() {
         `5,0,1
 1,5,1
 1,1,5`
+      );
+    });
+
+    it('linked back', function() {
+      assert.deepEqual(
+        parser(
+          `5,,1
+1,a1,1
+1,1,b2`
+        ),
+        `5,0,1
+1,5,1
+1,1,5`
+      );
+    });
+
+    it('sample', function() {
+      assert.deepEqual(
+        parser(`b1 b2 +,2 b2 3 * -,3 ,+
+a1,5 ,,7 2/
+c2 3*,1 2 , ,5 1 2+4*+3-`),
+        `-8,-13,3,#ERR
+-8,5,0,5
+0,#ERR,0,14`
       );
     });
   });
